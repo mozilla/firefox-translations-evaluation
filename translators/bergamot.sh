90e5573b36c1b26d9c693027324241cbef64935d
Evgeny Pavlov <epavlov@mozilla.com>
2022-01-28 16:23:56 -0800

    Fix bergamot translator (#9)

--- translators/bergamot.sh
+++ translators/bergamot.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-VOCAB=$(echo $MODEL_DIR/vocab.*.spm)
-MODEL=$(echo $MODEL_DIR/model.$SRC$TRG.intgemm.alphas.bin)
+VOCAB=$(echo "${MODEL_DIR}"/vocab.*.spm)
+MODEL="${MODEL_DIR}/model.${SRC}${TRG}.intgemm.alphas.bin"
+SHORTLIST="${MODEL_DIR}/lex.50.50.${SRC}${TRG}.s2t.bin"
+CONFIG="/tmp/bergamot.config.${SRC}${TRG}.yml"
 
-# These Marian options are set according to
-# https://github.com/mozilla-extensions/bergamot-browser-extension/blob/main/src/core/static/wasm/bergamot-translator-worker.appendix.js#L36
-# to imitate production setting
+cp translators/bergamot.config.yml "${CONFIG}"
+sed -i -e "s+MODEL+${MODEL}+g" "${CONFIG}"
+sed -i -e "s+VOCAB+${VOCAB}+g" "${CONFIG}"
+sed -i -e "s+SHORTLIST+${SHORTLIST}+g" "${CONFIG}"
 
-ARGS=(
-    --bergamot-mode wasm
-    -m $MODEL
-    --vocabs
-        $VOCAB # source-vocabulary
-        $VOCAB # target-vocabulary
-    --beam-size 1
-    --normalize 1.0
-    --word-penalty 0
-    --max-length-break 128
-    --mini-batch-words 1024
-    --workspace 128
-    --max-length-factor 2.0
-    --skip-cost
-    --cpu-threads 0
-    --quiet
-    --quiet-translation
-    --gemm-precision int8shift
-    --shortlist $MODEL_DIR/lex.50.50.$SRC$TRG.s2t.bin 50 50
-)
-
-$APP_PATH "${ARGS[@]}" $@
+$APP_PATH --model-config-paths "${CONFIG}"
